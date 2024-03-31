6c0ee4cf341871a5f22ec61c3e28637deb5a304e
浦田哲平 <urata@implem.co.jp>
2024-03-31 21:40:13 +0900

    既存処理のソースコードリファクタリング(一部未修整のものあり)。

--- 6. C#/作成中/ExportExcelFromJson/ExportExcelFromJson/Controller.cs
+++ 6. C#/作成中/ExportExcelFromJson/ExportExcelFromJson/Controller.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace ExportExcelFromJson
 {
@@ -7,13 +9,13 @@ namespace ExportExcelFromJson
         static void Main(string[] args)
         {
             var modelWriteToLogFile = new Model();
-            Dictionary<string, string> logType = new Dictionary<string, string> { { "INFO", "INFO   " },{ "WARNING", "WARNING"},{"ERROR", "WRROR  "} };
+            Dictionary<string, string> logType = new Dictionary<string, string> { { "INFO", "INFO   " }, { "WARNING", "WARNING" }, { "ERROR", "ERROR  " } };
             string jsonString;
             JObject jsonObject;
             Dictionary<string, string> statusCollection = new Dictionary<string, string>();
-            string logFile = "C:\\Implem\\個人フォルダ_PublicRepository\\PublicRepository\\6. C#\\作成中\\ExportExcelFromJson\\ExportExcelFromJson\\log.txt";
-            string inputFile = "C:\\Implem\\個人フォルダ_PublicRepository\\PublicRepository\\6. C#\\作成中\\ExportExcelFromJson\\ExportExcelFromJson\\multipleProcess.json";
-            string outputFile = "C:\\Implem\\個人フォルダ_PublicRepository\\PublicRepository\\6. C#\\作成中\\ExportExcelFromJson\\ExportExcelFromJson\\sample.xlsx";
+            string logFile = "C:\\Implem\\個人フォルダ_PublicRepository\\PublicRepository\\6. C#\\作成中\\ExportExcelFromJson\\ExportExcelFromJson\\bin\\Debug\\net8.0\\logs\\log.txt";
+            string inputFile = "C:\\Implem\\個人フォルダ_PublicRepository\\PublicRepository\\6. C#\\作成中\\ExportExcelFromJson\\ExportExcelFromJson\\bin\\Debug\\net8.0\\json\\multipleProcess.json";
+            string outputFile = "C:\\Implem\\個人フォルダ_PublicRepository\\PublicRepository\\6. C#\\作成中\\ExportExcelFromJson\\ExportExcelFromJson\\bin\\Debug\\net8.0\\excel\\sitesettings.xlsx";
             try
             {
                 modelWriteToLogFile.WriteToLogFile(logFile, "jsonファイルの読み取り：開始", logType["INFO"]);
@@ -31,24 +33,23 @@ namespace ExportExcelFromJson
                 }
                 else
                 {
-                foreach(var column in columns)
-                {
-                    string columnName = (string)column["ColumnName"];
-                    if (columnName != "Status") continue;
-                    string choicesText = (string)column["ChoicesText"];
-                    if (choicesText == null) continue; 
-                    var choicesTextArray = choicesText.Split("\n");
-                        var statusNum = choicesTextArray.Length;
-                        Console.WriteLine("ステータス数：" + statusNum);
+                    foreach (var column in columns)
+                    {
+                        string columnName = (string)column["ColumnName"];
+                        if (columnName != "Status") continue;
+                        string choicesText = (string)column["ChoicesText"];
+                        if (choicesText == null) continue;
+                        string[] choicesTextArray = choicesText.Split("\n");
                         statusCollection["0"] = "未設定";
                         statusCollection["-1"] = "全て";
                         int i = 1;
-                        foreach (var choiceText in choicesTextArray)
+                            foreach (var choiceText in choicesTextArray)
                         {
-                            var choiceTextArray = choiceText.Split(",");
+                            string[] choiceTextArray = choiceText.Split(",");
                             statusCollection[choiceTextArray[0]] = choiceTextArray[1];
-                            var modelManipulateExcel = new Model();
-                            modelManipulateExcel.ManipulateExcel(outputFile, choiceTextArray[1], 1, i, logFile, logType["INFO"], statusNum);
+                            Model modelManipulateExcel = new Model();
+                            int index = choicesTextArray.FindIndex(value => value == choiceTextArray[1]);
+                            modelManipulateExcel.ManipulateExcel(outputFile, choiceTextArray[1], 1, index, logFile, logType["INFO"]);
                             i++;
                         }
                     }
@@ -62,50 +63,72 @@ namespace ExportExcelFromJson
                 }
                 else
                 {
-                foreach(JObject process in processes)
-                {
-                    modelWriteToLogFile.WriteToLogFile(logFile, "プロセス設定の読み込み：開始", logType["INFO"]);
-                    try
+                    foreach (JObject process in processes)
                     {
-                        modelWriteToLogFile.WriteToLogFile(logFile, "Id:" + process["Id"], logType["INFO"]);
-                        modelWriteToLogFile.WriteToLogFile(logFile, "Name:" + process["Name"], logType["INFO"]);
-                        modelWriteToLogFile.WriteToLogFile(logFile, "DisplayName:" + process["DisplayName"], logType["INFO"]);
-                        modelWriteToLogFile.WriteToLogFile(logFile, "CurrentStatus" + process["CurrentStatus"], logType["INFO"]);
-                        modelWriteToLogFile.WriteToLogFile(logFile, "ChangedStatus" + process["ChangedStatus"], logType["INFO"]);
+                        modelWriteToLogFile.WriteToLogFile(logFile, "プロセス設定の読み込み：開始", logType["INFO"]);
+                        try
+                        {
+                            modelWriteToLogFile.WriteToLogFile(logFile, "Id:" + process["Id"], logType["INFO"]);
+                            modelWriteToLogFile.WriteToLogFile(logFile, "Name:" + process["Name"], logType["INFO"]);
+                            modelWriteToLogFile.WriteToLogFile(logFile, "DisplayName:" + process["DisplayName"], logType["INFO"]);
+                            modelWriteToLogFile.WriteToLogFile(logFile, "CurrentStatus" + process["CurrentStatus"], logType["INFO"]);
+                            modelWriteToLogFile.WriteToLogFile(logFile, "ChangedStatus" + process["ChangedStatus"], logType["INFO"]);
                             modelWriteToLogFile.WriteToLogFile(logFile, "現在の状況の比較処理：開始", logType["INFO"]);
-                            foreach (var status in statusCollection)
+                            int i = 1;
+                            foreach (var status in statusCollection.Select((Entry, Index) => new { Entry, Index }))
                             {
-                                if (status.Key == (string)process["CurrentStatus"]) 
+                                switch (status.Entry.Key)
                                 {
-                                    modelWriteToLogFile.WriteToLogFile(logFile, "現在の状況:" + status.Value, logType["INFO"]);
-                                    modelWriteToLogFile.WriteToLogFile(logFile, "Excelアクセス処理：開始", logType["INFO"]);
-                                    
-                                    break;
+                                    case "0":
+                                    case "-1":
+                                        modelWriteToLogFile.WriteToLogFile(logFile, "現在の状況が" + status.Entry.Value + "のためExcel操作処理をスキップ", logType["INFO"]);
+                                        break;
+                                    default:
+                                        if (status.Entry.Key == (string)process["CurrentStatus"])
+                                        {
+                                        modelWriteToLogFile.WriteToLogFile(logFile, "現在の状況:" + status.Entry.Value, logType["INFO"]);
+                                        Model modelManipulateExcel = new Model();
+                                        modelManipulateExcel.ManipulateExcel(outputFile, status.Entry.Value, ((int)process["Id"]+1), status.Index, logFile, logType["INFO"]);
+                                        }
+                                        break;
                                 }
+                                i = i + 1;
                             }
                             modelWriteToLogFile.WriteToLogFile(logFile, "現在の状況の比較処理：終了", logType["INFO"]);
                             modelWriteToLogFile.WriteToLogFile(logFile, "変更後の状況の比較処理：開始", logType["INFO"]);
-                            foreach (var status in statusCollection)
+                            int j = 1;
+                            foreach (var status in statusCollection.Select((Entry, Index) => new { Entry, Index }))
                             {
-                                if (status.Key == (string)process["ChangedStatus"])
+                                switch (status.Entry.Key)
                                 {
-                                    modelWriteToLogFile.WriteToLogFile(logFile, "変更後の状況:" + status.Value, logType["INFO"]);
-                                    break;
+                                    case "0":
+                                    case "-1":
+                                        modelWriteToLogFile.WriteToLogFile(logFile, "現在の状況が" + status.Entry.Value + "のためExcel操作処理をスキップ", logType["INFO"]);
+                                        break;
+                                    default:
+                                        if (status.Entry.Key == (string)process["CurrentStatus"])
+                                        {
+                                            modelWriteToLogFile.WriteToLogFile(logFile, "変更後の状況:" + status.Entry.Value, logType["INFO"]);
+                                            Model modelManipulateExcel = new Model();
+                                            modelManipulateExcel.ManipulateExcel(outputFile, status.Entry.Value, ((int)process["Id"] + 1), status.Index, logFile, logType["INFO"]);
+                                        }
+                                        break;
                                 }
+                                j = j + 1;
                             }
                             modelWriteToLogFile.WriteToLogFile(logFile, "変更後の状況の比較処理：終了", logType["INFO"]);
-                　　　　　　modelWriteToLogFile.WriteToLogFile(logFile, "プロセスの取得処理：終了", logType["INFO"]);
+                            modelWriteToLogFile.WriteToLogFile(logFile, "プロセスの取得処理：終了", logType["INFO"]);
+                        }
+                        catch (Exception e)
+                        {
+                            modelWriteToLogFile.WriteToLogFile(logFile, "例外が発生しました。", logType["ERROR"]);
+                            Console.WriteLine(e);
+                        }
+                        finally
+                        {
+                        }
                     }
-                    catch (Exception e)
-                    {
-                        modelWriteToLogFile.WriteToLogFile(logFile, "例外が発生しました。", logType["ERROR"]);
-                        Console.WriteLine(e);
-                    }
-                    finally
-                    {
-                    }
-                }
-                modelWriteToLogFile.WriteToLogFile(logFile, "プロセス設定の読み込み：終了", logType["INFO"]);
+                    modelWriteToLogFile.WriteToLogFile(logFile, "プロセス設定の読み込み：終了", logType["INFO"]);
                 }
             }
             catch (Exception e)
--- 6. C#/作成中/ExportExcelFromJson/ExportExcelFromJson/Model.cs
+++ 6. C#/作成中/ExportExcelFromJson/ExportExcelFromJson/Model.cs
@@ -1,6 +1,6 @@
 ﻿using Excel = Microsoft.Office.Interop.Excel;
 using System;
-using System.IO;
+using io = System.IO;
 using System.Text;
 
 
@@ -8,33 +8,37 @@ namespace ExportExcelFromJson
 {
     internal class Model
     {
-        public void ManipulateExcel (string ExportFile, string status, int line, int column, string logFile, string logType, int statusNum)
+        public void ManipulateExcel(string ExportFile, string text, int line, int column, string logFile, string logType)
         {
-            Console.WriteLine(column);
             WriteToLogFile(logFile, "Excelアクセス処理：開始", logType);
-            Excel.Application excelApp = new Excel.Application();
-            Excel.Workbooks excelBooks = excelApp.Workbooks;
-            Excel.Workbook excelBook = excelBooks.Add();
-            Excel.Worksheet sheet = (Excel.Worksheet)excelApp.Worksheets["sheet1"];
+            WriteToLogFile(logFile, "Cells[" + line + ", " + column + "] = " + text, logType);
+            Excel.Application excelApp = new Excel.Application(); ;
+            Excel.Workbooks excelBooks = excelApp.Workbooks; ;
+            Excel.Workbook excelBook = null;
+            Excel.Sheets sheets = null;
+            Excel.Worksheet sheet = null;
+            excelApp.Visible = false;
+            excelApp.DisplayAlerts = false;
             try
             {
-            WriteToLogFile(logFile, "Excel操作処理：開始", logType);
-                if (File.Exists(ExportFile))
+                WriteToLogFile(logFile, "Excel操作処理：開始", logType);
+                if (!File.Exists(ExportFile))
                 {
-                    WriteToLogFile(logFile, "既存ブックが存在する場合", logType);
-                    excelApp.Visible = false;
-                    excelApp.DisplayAlerts = false;
-                    sheet.Cells[line, column] = status;
-                    excelBook.Save();
+                    WriteToLogFile(logFile, "既存ブックが存在しない場合", logType);
+                    excelBook = excelBooks.Add();
+                    sheet = (Excel.Worksheet)excelApp.Worksheets["sheet1"];
+                    sheet.Cells[line, column] = text;
+                    excelBook.SaveAs(ExportFile);
+                    WriteToLogFile(logFile, ExportFile, logType);
                 }
                 else
                 {
-                    WriteToLogFile(logFile, "既存ブックが存在しない場合", logType);
-                    excelApp.Visible = false;
-                    excelApp.DisplayAlerts = false;
-                    sheet.Cells[line, column] = status;
-                    excelBook.SaveAs(ExportFile);
-                    WriteToLogFile(logFile, ExportFile , logType);
+                    WriteToLogFile(logFile, "既存ブックが存在する場合", logType);
+                    excelBook = excelBooks.Open(Path.GetFullPath(ExportFile));
+                    sheets = excelBook.Worksheets;
+                    sheet = sheets[1];
+                    sheet.Cells[line, column] = text;
+                    excelBook.Save();
                 }
             }
             catch (Exception e)
@@ -44,25 +48,51 @@ namespace ExportExcelFromJson
             }
             finally
             {
-                if (column == statusNum)
-                {
                 WriteToLogFile(logFile, "Excel操作処理：終了", logType);
                 excelApp.Quit();
                 System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet);
                 System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBooks);
                 System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
                 WriteToLogFile(logFile, "Excelアクセス処理：終了", logType);
-                }
             }
         }
 
-        public void WriteToLogFile (string File, string message, string logType)
+        public void WriteToLogFile(string File, string text, string logType)
         {
-            message = logType + "【" + DateTime.Now + "】" +   message;
+            string message = logType + "【" + DateTime.Now + "】" + text;
             Encoding enc = Encoding.GetEncoding("utf-8");
-            using (StreamWriter writer = new StreamWriter(File, true, enc))
+            try
             {
-                writer.WriteLine(message);
+                if (io.File.Exists(File))
+                {
+                    using (StreamWriter writer = new StreamWriter(File, true, enc))
+                    {
+                        writer.WriteLine(message);
+                    }
+                }
+                else
+                {
+                    using (var fileStream = io.File.Create(File))
+                    {
+                        //書き込み
+                        byte[] info = new UTF8Encoding().GetBytes(message + "\n");
+                        fileStream.Write(info, 0, info.Length);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                message = "ERROR  【" + DateTime.Now + "】例外が発生しました。";
+                using (StreamWriter writer = new StreamWriter(File, true, enc))
+                {
+                    writer.WriteLine(message);
+                }
+                Console.WriteLine(e);
+            }
+            finally
+            {
+
             }
         }
     }
